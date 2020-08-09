7239294028d95f4e509efd664e08496260bb6e1c
Ashfaq H Ahmed <ashfaq@pinesolutions.xyz>
2020-08-09 17:50:19 +0600

    Will start with routing in the app.

---
+++ src/components/CounterExample.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+function CounterExample() {
+    const [count, setCount] = useState(0)
+    // console.log(useState(0))
+    return (
+        <div>
+            <h1>Count: {count}</h1>
+
+            <h1 onClick={() => setCount(count + 1)} >Plus</h1>
+            <h1 onClick={() => setCount(count - 1)} >minus</h1>
+        </div>
+    )
+}
+export default CounterExample
